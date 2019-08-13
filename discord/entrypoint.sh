ed9a31891622be9ecab5e6e59ee0f794f360777f
Zachary Williams <zlw11063@gmail.com>
2019-08-13 10:18:42 -0600

    attempt

--- discord/entrypoint.sh
+++ discord/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-curl -H "Content-Type: application/json" -X POST -d '{"username": "Github Actions", "content": "New changes to ${GITHUB_REPOSITORY} have been pushed to master"}' $DISCORD_WEBHOOK
+curl -H "Content-Type: application/json" -X POST -d `{"username": "Github Actions", "content": "New changes to ${GITHUB_REPOSITORY} have been pushed to master"}` $DISCORD_WEBHOOK

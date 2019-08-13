149dace3d5dd75806249cfdf75f889977da8fdae
Zachary Williams <zlw11063@gmail.com>
2019-08-13 08:52:22 -0600

    attempt

--- discord/entrypoint.sh
+++ discord/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
 
+sudo apt-get install curl
 curl -H "Content-Type: application/json" -X POST -d '{"username": "test", "content": "hello"}' https://discordapp.com/api/webhooks/610837816607506434/y2gsl4hX7DBG2dqKX9UN2qjSP6LfOK9TlqM2Yoca1e3TOYbPlbUO1mhzA1LhLsNsS8ev

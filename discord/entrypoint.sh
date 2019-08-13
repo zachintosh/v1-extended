8d2420834cc79dd0bb7d63bb084c1139b7e62981
Zachary Williams <zlw11063@gmail.com>
2019-08-13 08:50:12 -0600

    attempt

---
+++ discord/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -l
+
+curl -H "Content-Type: application/json" -X POST -d '{"username": "test", "content": "hello"}' https://discordapp.com/api/webhooks/610837816607506434/y2gsl4hX7DBG2dqKX9UN2qjSP6LfOK9TlqM2Yoca1e3TOYbPlbUO1mhzA1LhLsNsS8ev
